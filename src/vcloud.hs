ebb6e5e2f553fbf9d73126b53ef70835a7eddb25
Pierre Radermecker <pierrer@pi3r.be>
2016-02-01 17:44:21 +0100

    Add code comments

--- src/vcloud.hs
+++ src/vcloud.hs
@@ -42,21 +42,26 @@ main = do
       -- query a specific virtual machine
   S.withSessionControl (Just (HTTP.createCookieJar [])) ignoreTLSCertificatesSettings $ \sess -> do
     let opts = defaults & header "Accept" .~ ["application/*+xml;version=" <> vCloudVers]
+    -- check action
+    (contentType, payload) <- lookupPayload vmAction
+    -- login
     _ <- S.getWith (opts & auth ?~ basicAuth vCloudUser vCloudPass) sess loginPath
+    -- Get raw xml info about our vApp
     raw0 <- S.getWith opts sess vAppQueryPath
     let vmId = raw0 ^. responseBody . fetchVM vmName . fetchVmId.text
         actionUri = apiUrl <> "/vApp/vm-" <> Text.unpack vmId <> "/action/" <> vmAction
-    (contentType, payload) <- lookupPayload vmAction
     let postOpts = maybe opts (\x -> opts & header "Content-Type" .~ [x]) contentType
+    -- post the action
     raw1 <- S.postWith postOpts sess actionUri payload
     print $ raw1 ^. responseStatus.statusMessage
     let taskLink = raw1 ^. responseHeader "Location"
-        actionStatus n  = putStrLn "Waiting for status"
+        waitSuccess n  = putStrLn "Waiting for status"
                           *> threadDelay (n*1000*1000)
                           *> do
                              r <- S.getWith opts sess (BC.unpack taskLink)
                              case r ^. responseBody .xml.attr "status" of
                                Just "success" -> putStrLn "success"
-                               Just "running" -> actionStatus 10
+                               Just "running" -> waitSuccess n
                                _              -> putStrLn "fails"
-    actionStatus 10
+    -- wait for a success return code
+    waitSuccess 12
