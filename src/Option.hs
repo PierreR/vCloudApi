4b701c9714058ce3ead15868522c64a33f0b55e1
Pierre Radermecker <pierrer@pi3r.be>
2016-02-02 12:15:12 +0100

    Remove some defaults for cmd options

--- src/Option.hs
+++ src/Option.hs
@@ -18,35 +18,33 @@ data Options = Options
 options :: Parser Options
 options = Options
     <$> strOption
-        (long "vCloudURL"
+        ( long "vCloudURL"
         <> short 'h'
         <> help "VCloud base URL"
         <> value "https://c.irisnet.be")
     <*> ( BC.pack <$> strOption
-        (  long "user"
+        ( long "user"
         <> short 'u'
         <> help "Your user name and org ex.: myuser@cirb-test"))
     <*> ( BC.pack <$> strOption
-        (  long "password"
+        ( long "password"
         <> short 'p'
         <> help "Your password"))
     <*> strOption
-        (long "vAppId"
+        ( long "vAppId"
         <> short 'q'
-        <> help "vApp id (default is 'f1ce1ecb-af39-41d3-ac9c-21deecb4e23d')"
-        <> value "f1ce1ecb-af39-41d3-ac9c-21deecb4e23d")
-    <*> (Text.pack <$> strOption
-        (long "vmName"
+        <> help "vApp id (such as 'f1ce1ecb-af39-41d3-ac9c-21deecb4e23d')")
+    <*> ( Text.pack <$> strOption
+        ( long "vmName"
         <> short 'n'
-        <> help "virtual machine name (default is 'saltsyndic')"
-        <> value "saltsyndic"))
+        <> help "virtual machine name (such as 'saltsyndic')"))
     <*> strOption
-        (long "vmAction"
+        ( long "vmAction"
         <> short 'a'
         <> help "virtual machine action to perform against the API (ex: revertToCurrentSnapshot)")
 
 cmdOpts :: IO Options
 cmdOpts = execParser $ info (helper <*> options)
-        (fullDesc
-         <> progDesc "VCloud APIs command line wrapper"
+        ( fullDesc
+        <> progDesc "VCloud APIs command line wrapper"
         )
